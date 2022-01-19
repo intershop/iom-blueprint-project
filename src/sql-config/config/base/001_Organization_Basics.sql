f2ed5f0c7bee5ac087c16232dcd58f451fdabaf2
Martin Krippl <97231647+mkri-ish@users.noreply.github.com>
2022-01-19 18:49:48 +0100

    initial commit for organization and shops

---
+++ src/sql-config/config/base/001_Organization_Basics.sql
@@ -0,0 +1,25 @@
+SET client_min_messages=error;
+#parse("src/sql-config/config/vars.vm")
+-- import global variables
+$vars_shop_supplier
+
+-- PAUSE velocity parser
+#[[
+-- local variables go here
+
+-- end variables block with 
+BEGIN
+
+IF NOT EXISTS(SELECT null from oms."OrganizationDO" where "name" = 'inSPIRED') THEN
+	PERFORM admin.createdomain
+	(
+	    'OmsSystem',                    --parent_org_name varchar,
+	    'inSPIRED',    						--name            varchar,
+		'inSPIRED Organization'				--description     varchar
+	);
+END IF;
+
+END;
+]]#
+-- dollar quoting
+$do;
