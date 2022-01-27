5cc248095d85134697ef63c14da7be8bd4aa9fcc
Martin Krippl <97231647+mkri-ish@users.noreply.github.com>
2022-01-27 15:09:30 +0100

    #73688: added missing sql function for carrier

---
+++ src/sql-config/dbmigrate/204_create_or_update_carrier.sql
@@ -0,0 +1,26 @@
+DO $$
+BEGIN
+        
+    CREATE OR REPLACE FUNCTION oms.create_or_update_carrier(
+        p_id bigint,
+        p_name text,
+        p_trackingUrl text)
+      RETURNS void AS
+    $BODY$BEGIN
+
+        IF NOT EXISTS (SELECT null FROM "CarrierDO" WHERE id = p_id) THEN
+            INSERT INTO oms."CarrierDO" (id, name, "trackingUrl", "modificationDate", version)
+                SELECT p_id, p_name, p_trackingUrl, now(), 0;
+        ELSE
+            UPDATE "CarrierDO" SET name = p_name, "trackingUrl" = p_trackingUrl WHERE id = p_id;
+            
+        END IF;
+
+
+            END;
+            $BODY$
+      LANGUAGE plpgsql VOLATILE
+      COST 100;
+
+END;
+$$;
