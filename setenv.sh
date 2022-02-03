d188b40deffae2a2f1775d883041d3f5e78f3cd0
Martin Krippl <97231647+mkri-ish@users.noreply.github.com>
2022-02-03 19:09:45 +0100

    #73688: added CUSTOM_*_DIR properties, adjusted setevn.sh

--- setenv.sh
+++ setenv.sh
@@ -3,5 +3,5 @@ kubectl config use-context docker-desktop
 mkdir -p devenv-share
 #export DEVENV4IOM_CONFIG="$(pwd)/devenv-config.properties"
 export PATH="$(pwd)/devenv-4-iom/bin:$PATH"
-export DEVENV_SHARE_BASE_PATH="/run/desktop/mnt/host$(pwd)"
+export DEVENV_SHARE_BASE_PATH="$(pwd)"
 
