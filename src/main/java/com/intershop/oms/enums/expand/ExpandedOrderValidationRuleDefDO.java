02c329863aee380659639274be104675df7defa8
Jan Listemann <j.listemann@intershop.de>
2022-03-15 21:44:40 +0100

    added order validation rules

--- src/main/java/com/intershop/oms/enums/expand/ExpandedOrderValidationRuleDefDO.java
+++ src/main/java/com/intershop/oms/enums/expand/ExpandedOrderValidationRuleDefDO.java
@@ -1,5 +1,7 @@
 package com.intershop.oms.enums.expand;
 
+import java.util.EnumSet;
+
 import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.Transient;
@@ -8,14 +10,16 @@ import bakery.persistence.annotation.PersistedEnumerationTable;
 import bakery.persistence.dataobject.configuration.common.OrderValidationRuleDefDO;
 import bakery.persistence.expand.OrderValidationRuleDefDOEnumInterface;
 
-@PersistedEnumerationTable(OrderValidationRuleDefDO.class)
+@PersistedEnumerationTable( OrderValidationRuleDefDO.class )
 public enum ExpandedOrderValidationRuleDefDO implements OrderValidationRuleDefDOEnumInterface
 {
-
     /**
-     * Minimum ID for custom entries: 1000 length restriction for name: 50
+     * Minimum ID for custom entries: 10000 length, restriction for name: 50
+     * The name must be unique across both classes.
+     * Values with negative id are meant as syntax example and are ignored (won't get persisted within the db).
      */
-    VALIDATE_PROPERTIES(-9999, "ValidateMandatoryPropertiesPTBean", "java:global/example-app/ValidateMandatoryPropertiesPTBean!bakery.logic.service.order.task.ValidateOrderPT", 999, false)
+    SYNCROUNOUS_PROPERTY_VALIDATION (Integer.valueOf(10000), "ValidateCustomOrderPropertiesSyncrPTBean", "java:global/iom-blueprint-project/ValidateCustomOrderPropertiesSyncrPTBean!bakery.logic.service.order.task.ValidateOrderPT", 1000, false, "Validation fails if custom order level property group|key|value = order|validation|fail-syncrounously is given."),
+    ASYNCROUNOUS_PROPERTY_VALIDATION(Integer.valueOf(10001), "ValidateCustomOrderPropertiesASyncrPTBean", "java:global/iom-blueprint-project/ValidateCustomOrderPropertiesASyncrPTBean!bakery.logic.service.order.task.ValidateOrderPT", 1001, false, "Validation fails if custom order level property group|key|value = validation|fail-asyncrounously-autocancel is given.")
     ;
     
     private Integer id;
@@ -23,19 +27,18 @@ public enum ExpandedOrderValidationRuleDefDO implements OrderValidationRuleDefDO
     private String jndiName;
     private int rank;
     private boolean mandatory;
+    private String description;
 
-    private ExpandedOrderValidationRuleDefDO(Integer id, String name, String jndiName, int rank, boolean mandatory)
+    private ExpandedOrderValidationRuleDefDO(Integer id, String name, String jndiName, int rank, boolean mandatory, String description)
     {
         this.id = id;
         this.name = name;
         this.jndiName = jndiName;
         this.rank = rank;
         this.mandatory = mandatory;
+        this.description = description;
     }
 
-    /**
-     * Id der Pruefungsart
-     */
     @Override
     @Id
     public Integer getId()
@@ -43,44 +46,49 @@ public enum ExpandedOrderValidationRuleDefDO implements OrderValidationRuleDefDO
         return this.id;
     }
 
-    /**
-     * Id der Pruefungsart
-     */
     protected void setId(Integer id)
     {
         this.id = id;
     }
 
-    /**
-     * Namen der Pruefungsregel
-     */
     @Override
-    @Column(name = "`name`", length = 50, nullable = false)
+    @Column(name = "name", length = 50, nullable = false)
     public String getName()
     {
         return this.name;
     }
 
-    /**
-     * Namen der Pruefungsregel
-     */
     public void setName(String name)
     {
         this.name = name;
     }
 
+    @Column(name = "description")
+    public String getDescription()
+    {
+        return this.description;
+    }
+
+    @SuppressWarnings("unused")
+    protected void setDescription(String description)
+    {
+        // dummy setter for the needs of hibernate
+    }
+
     /**
-     * Ranking (Reihenfolge) der Pruefung
+     * Priority the rule should be checked.
      */
     @Override
-    @Column(name = "`rank`", nullable = false)
+    @Column(name = "rank", nullable = false)
     public int getRank()
     {
         return this.rank;
     }
 
     /**
-     * Ranking (Reihenfolge) der Pruefung
+     * Priority the rule should be checked.
+     * 
+     * @param rank
      */
     public void setRank(int rank)
     {
@@ -88,20 +96,18 @@ public enum ExpandedOrderValidationRuleDefDO implements OrderValidationRuleDefDO
     }
 
     /**
-     * Gibt an ob dieser Parameter abgeschaltet werden darf.
-     *
-     * @return <b>true</b> oder <b>false</b>
+     * Whether the rule is mandatory and must not be disabled.
      */
     @Override
-    @Column(name = "`mandatory`", nullable = false)
+    @Column(name = "mandatory", nullable = false)
     public boolean isMandatory()
     {
         return this.mandatory;
     }
 
     /**
-     * Gibt an ob dieser Parameter abgeschaltet werden darf.
-     *
+     * Whether the rule is mandatory and must not be disabled.
+     * 
      * @param mandatory
      *            <b>true</b> oder <b>false</b>
      */
@@ -114,7 +120,18 @@ public enum ExpandedOrderValidationRuleDefDO implements OrderValidationRuleDefDO
     @Transient
     public String getJndiName()
     {
-        return this.jndiName;
+        return String.format(this.jndiName, bakery.util.DeploymentConfig.APP_VERSION);
     }
 
+    @Transient
+    public final EnumSet<ExpandedOrderValidationRuleDefDO> getExpandedEnums()
+    {
+        return EnumSet.allOf(ExpandedOrderValidationRuleDefDO.class);
+    }
+
+    @Transient
+    public final EnumSet<OrderValidationRuleDefDO> getAllEnums()
+    {
+        return EnumSet.allOf(OrderValidationRuleDefDO.class);
+    }
 }
