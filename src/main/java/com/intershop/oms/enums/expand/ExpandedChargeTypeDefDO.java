eadd6daa1466696ef76694b8e626d279d732b5b8
Marc Mamin <m.mamin@intershop.de>
2022-04-25 11:16:38 +0200

    #73840 fix name for OMT

--- src/main/java/com/intershop/oms/enums/expand/ExpandedChargeTypeDefDO.java
+++ src/main/java/com/intershop/oms/enums/expand/ExpandedChargeTypeDefDO.java
@@ -13,8 +13,8 @@ public enum ExpandedChargeTypeDefDO implements ChargeTypeDefDOEnumInterface
 {
     // start with 1000 to avoid conflicts with ChargeTypeDefDO
     // the name must be unique across both classes
-	CONTAINER_SERVICE_CHARGE(Integer.valueOf(1000), "Container Service Charge", "tb.chargetype.containercharge", "Container Service Charges");
-    
+	CONTAINER_SERVICE_CHARGE(Integer.valueOf(1000), "Container Service Charge", "Container Charge", "Container Service Charges");
+
 
 
     private Integer id;
