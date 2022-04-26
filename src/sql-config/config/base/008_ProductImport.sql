ae34a1ecc54446322162212a33fa5721c8141d0f
Marc Mamin <m.mamin@intershop.de>
2022-04-26 14:43:30 +0200

    remove reference to priceComparisonProvider (is null anyway)

--- src/sql-config/config/base/008_ProductImport.sql
+++ src/sql-config/config/base/008_ProductImport.sql
@@ -15,13 +15,13 @@ LOOP
 	INSERT INTO product."ImportConfigurationDO"(
 	            "id", "availabilityStoredProcedure", "copyStoredProcedure", "csvDelimiter", --1
 				"csvHeader", 
-				"priceComparisonProviderDefRef", "storedProcedureGroup", "supplierRef",  "tableNameGroup", --4
+				"storedProcedureGroup", "supplierRef",  "tableNameGroup", --4
 				"checkMissingInLastDatapack", "importDatapackFileTypeDefRef", "canDatapackBeEmpty", "parentImportConfigurationRef", "convertToCSV", "createContentAssignment", --5
 	            "internalMediaPath", "splitCount", "variationProcedure", "identifyOnBakeryArticleRef", "supplementArticleParent", --6
 				"assortmentSupplier", "updateArticleType", "createManufacturer", "createSupplierAssortmentSystem", "csvQuote") --7
 	    		SELECT nextval('"ImportConfigurationDO_id_seq"'), '', '', '''|''', --1
 					'supplierArticleNo|manufacturer|manufacturerArticleNo|ISBN|EAN|articleName|length|height|width|weight|assortmentName|assortmentIdentifier|deliveryForm|customsTariffNo|parentSupplierArticleNo|articleForm|immaterialUid|articleLanguage|supplierSalesCode|supplierArticleIdentifier|articleType|edition|packagingUnit|packagingUnitValue', 
-					NULL, 'import_standard_a_v10', supplierref, 'Import_Standard_Basic_V10', --4
+					'import_standard_a_v10', supplierref, 'Import_Standard_Basic_V10', --4
 					FALSE, 1, TRUE, NULL, FALSE, FALSE, --5
 		            '', 6, NULL, FALSE, TRUE, --6
 					supplierref, FALSE, TRUE, TRUE, E'\30'; --7
@@ -30,7 +30,7 @@ LOOP
 	INSERT INTO "ImportConfigurationDO" (
 				id,	"availabilityStoredProcedure", "copyStoredProcedure","csvDelimiter", --1
 				"csvHeader", --2
-				"priceComparisonProviderDefRef", "storedProcedureGroup", --4
+				"storedProcedureGroup", --4
 				"supplierRef", "tableNameGroup", "checkMissingInLastDatapack", --5
 				"importDatapackFileTypeDefRef",	"canDatapackBeEmpty", "parentImportConfigurationRef", --6
 				"convertToCSV", "createContentAssignment", "internalMediaPath", --7
@@ -39,7 +39,7 @@ LOOP
 				"importBeanDefRef", "createSupplierAssortmentSystem","createManufacturer", "financeControllerRef", "csvQuote") --10
 			SELECT nextval('"ImportConfigurationDO_id_seq"'), '', '', '''|''', --1
 					'supplierArticleNo|bakeryArticleNo|shopArticleNo',	--2
-					NULL, 'import_standard_n_v4', --4
+					'import_standard_n_v4', --4
 					supplierref, 'Import_Standard_ShopArticleNo_V4', false, --5
 					39,	false, NULL, --6
 					false,	false,	'', --7
@@ -51,13 +51,13 @@ LOOP
 	INSERT INTO product."ImportConfigurationDO"(
 	            "id", "availabilityStoredProcedure", "copyStoredProcedure", "csvDelimiter", --1 
 				"csvHeader", 
-				"priceComparisonProviderDefRef", "storedProcedureGroup", "supplierRef", --4 
+				"storedProcedureGroup", "supplierRef", --4 
 				"tableNameGroup", "checkMissingInLastDatapack", "importDatapackFileTypeDefRef", "canDatapackBeEmpty", --5
 	            "parentImportConfigurationRef", "convertToCSV", "createContentAssignment", "internalMediaPath", "splitCount", --6
 				"variationProcedure", "identifyOnBakeryArticleRef", "supplementArticleParent", "assortmentSupplier", "updateArticleType", "csvQuote") --7
 	    		SELECT nextval('"ImportConfigurationDO_id_seq"'), 'process_availability_mapping_do_nothing', '', '''|''', -- 1 
 					'supplierArticleNo|currency|purchasePrice|listPrice|stockLevel|stockType|availabilityInDays|salesPrice|basicPrice|basicPriceUnitValue|basicPriceUnit|salesPriceOld|provisionType|provisionPercentage|taxType|isBestseller', 
-					NULL, 'import_standard_bcg_v3', supplierref, --4
+					'import_standard_bcg_v3', supplierref, --4
 					'Import_Standard_Dynamic_V7', FALSE, 13, TRUE, --5
 		            NULL, FALSE, FALSE, '', 6, --6 
 					NULL, FALSE, TRUE, supplierref, FALSE, E'\30'; --7
