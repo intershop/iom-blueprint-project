89f5fcd0ec6819920da349601948d9afdc2662bf
Jan Listemann <j.listemann@intershop.de>
2022-06-07 11:38:25 +0200

    #77143 enabled OrderValidationRule#6 again due to bugfix in IOM 4.1.1

--- src/sql-config/config/base/010_Validation.sql
+++ src/sql-config/config/base/010_Validation.sql
@@ -17,7 +17,7 @@ BEGIN
 		FOR rec IN (
 			select 4  as rule_id,   FALSE as synchron	union all 
 			select 5,           	FALSE          		union all 
-			--select 6,           	TRUE           		union all --comment in if ESC is fixed https://dev.azure.com/intershop-com/cefd1005-00a7-4c79-927f-a16947d1b2e6/_workitems/edit/77227
+			select 6,           	TRUE           		union all
 			select 11,          	FALSE          		union all 
 			select 13,          	TRUE           		union all 
 			select 10000,       	TRUE           		-- custom rule syncrounous
